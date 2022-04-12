3cd32724b49e5d80195ba03916d06f9ee08a8c98
Nonso <amnonso@yahoo.com>
2022-04-12 06:14:08 -0700

    nested alhpabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - check the code.
+ *Return: Always 0.
+ **/
+void print_alphabet_x10(void)
+{ int l;
+  int n = 0;
+
+  while(n < 10)
+  { l = 'a';
+	while(l <= 'z')
+	{
+	 _putchar(l);
+	 l += 1;
+	}
+	_putchar(10);
+	n += 1;
+  }
+}
