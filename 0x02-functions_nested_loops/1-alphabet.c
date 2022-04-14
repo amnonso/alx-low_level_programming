22e842bd26abd61b5d11eb5abaf92a17212d1898
Nonso <amnonso@yahoo.com>
2022-04-14 05:57:04 -0700

    edited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  *print_alphabet - function that prints the alphabet, lowercase
  *You can only use _putchar twice in your code
  *Return: nothing.
  **/
-
 void print_alphabet(void)
 {
     int l = 'a';
