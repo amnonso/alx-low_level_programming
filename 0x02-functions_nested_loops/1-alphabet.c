783bc2396909fad28f904a92763e03ace19788ed
Nonso <amnonso@yahoo.com>
2022-04-14 06:33:02 -0700

    another

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,12 +10,11 @@ void print_alphabet(void)
     int l = 'a';
    
     while (l <= 'z')
-       
-    {
+       	 {
           _putchar(l);
          
 	  l += 1;
-       }
+       	}
     
     _putchar(10);
 
