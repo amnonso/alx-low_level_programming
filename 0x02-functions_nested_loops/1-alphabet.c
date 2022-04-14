8f1bcb547b11b90949da83882b8e3aa84c3887fd
Nonso <amnonso@yahoo.com>
2022-04-14 06:12:11 -0700

    try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,18 @@
  *Return: nothing.
  **/
 void print_alphabet(void)
+
 {
     int l = 'a';
+   
     while (l <= 'z')
-       {
+       
+    {
           _putchar(l);
-          l += 1;
-       }
-     _putchar(10);
+         
+	  l += 1;
+      
+    }
+    
+    _putchar(10);
 }
