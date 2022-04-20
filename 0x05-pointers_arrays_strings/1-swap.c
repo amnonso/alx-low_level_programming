c32bbcf53550d232cdcd96d5d164cc3ce3f3322a
amnonso <41832631+amnonso@users.noreply.github.com>
2022-04-20 23:31:08 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,35 +1,18 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * swap_int - swaps the values of two integers.
  *
- *  *swap_int - swap numbers function
- *
- *   *@a: integer
- *
- *    *@b: integer
- *
- *     *
- *
- *      *Return: nothing
- *
- *       */
-
-
+ * @a: pointer to an int
+ * @b: pointer to other int
+ * Return: Nothing
+ */
 
 void swap_int(int *a, int *b)
-
 {
+	int aux;
 
-	int temp;
-
-
-
-	temp = *a;
-
+	aux = *a;
 	*a = *b;
-
-	*b = temp;
-
+	*b = aux;
 }
